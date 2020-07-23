17e0e69e1e2094a03ca3408096925d1b0ce3fd23
Nathan <29925490+NathanD-02@users.noreply.github.com>
2020-07-23 17:33:40 +0100

    Updated version of CovidTracker.py
    This version of the program has slightly different messages. Additionally, you do not need to restart the program to view more data, you are asked if you wish to view another one.

--- CovidTracker.py
+++ CovidTracker.py
@@ -1,18 +1,22 @@
 from covid import Covid
+def tracker():
+    covid = Covid()
+    country = input("Enter country you wish to view COVID-19 data for: ")
+    try:
+     data = covid.get_status_by_country_name(country)
+     print(f"Showing details for {country}")
+     for key in data:
+            print(f"{key} : {data[key]}")
 
-covid = Covid()
+    except ValueError:
+        print("Country not found, please check input.")
+        print("Try alternative spellings of names. Sub-countries like Wales may not work")
+    answer = input("View another countries COVID data? If you choose N, program ends. Y/N")
 
-country = input("Enter you country name : ")
+    if answer == 'N' or answer == 'n':
+        print("Exiting program...")
+        
+    elif answer == 'Y' or answer == 'y':
+        tracker()
 
-try: 
-    data = covid.get_status_by_country_name(country)
-
-    print(f"Showing details of {country}")
-    
-    for key in data:
-        print(f"{key} : {data[key]}")
-
-except ValueError:
-    print("Country not found, please check your input!")
-
-
+tracker()
